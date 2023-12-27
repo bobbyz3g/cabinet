d99fe1a459b7f6e4f31eacd168fe3f97c61ae989
bobby <zhangliang925@icloud.com>
2023-12-27 22:53:27 +0800

    :sparkles: add file download functionality for cabinet package

--- cmd/cabinet/main.go
+++ cmd/cabinet/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"net/http"
 
 	"github.com/bobbyz3g/cabinet"
@@ -18,6 +19,23 @@ func main() {
 			return err
 		}
 		return c.String(http.StatusCreated, "ok")
+	})
+
+	e.GET("/file/:code", func(c echo.Context) error {
+		code := cabinet.Code(c.Param("code"))
+		t, ok := sessions.Pop(code)
+		if !ok {
+			return echo.NewHTTPError(http.StatusNotFound, "translator not found")
+		}
+		defer close(t.Done)
+
+		c.Response().Header().Set("Content-Disposition", "attachment; filename="+t.Name)
+		c.Response().Header().Set("Content-Type", "application/octet-stream")
+		_, err := io.Copy(c.Response(), t.Reader)
+		if err != nil {
+			return err
+		}
+		return nil
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
